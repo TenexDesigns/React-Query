b94f9329977ca91837916d44229c14bd8272cc04
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-22 14:12:11 +0300

    Update and rename 1_Cacing.java to 1_Caching.java

---
+++ Caching/1_Caching.java
@@ -0,0 +1,143 @@
+React query provides a caching for 5 minites, with the kesy of the uniwur key e.g superhereos and the call back function passed to the useQury
+
+
+ const { isLoading, error, isError,data } = useQuery('super-hero', fetchData);
+
+        REat query also provides a background refetch for the same qury to avoid stale data
+
+
+
+CUSTOMISE CCHING TIME
+
+
+
+ const { isLoading, error, isError,data } = useQuery('super-hero', fetchData, { cacheTime: 5000});
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------
+
+ 
+In React Query, caching is a fundamental feature that helps optimize the performance of your application by storing and managing the results of data queries. React Query provides a sophisticated caching system that handles data fetching, storage, and invalidation, making it easier to work with remote data in your React components.
+
+Here's a brief overview of how caching works in React Query:
+
+### 1. Query Cache:
+
+React Query maintains a query cache that stores the results of your data queries. When you make a query, the result is stored in this cache, and subsequent requests for the same data can be served directly from the cache without making a network request.
+
+### 2. Automatic Invalidation:
+
+React Query automatically invalidates cached data under certain conditions, such as when data is refetched or when mutations (changes to the data) occur. This ensures that the cached data stays up-to-date.
+
+### 3. Time-Based Expiry:
+
+You can configure caching options, including time-based expiry. This means that you can set a time limit for how long the data should be considered valid. After this time limit, React Query will automatically refetch the data.
+
+### 4. Manual Invalidation:
+
+You have the ability to manually invalidate or refetch specific queries or portions of the cache when needed. This can be useful in scenarios where you want to force a refresh of certain data.
+
+### Where to Use Caching in React Query:
+
+1. **Queries:**
+   - You can use caching for queries that fetch data from remote sources. When you define a query using `useQuery`, React Query automatically manages the caching of the query results.
+
+    ```jsx
+    const { data, error, isLoading } = useQuery('myData', fetchDataFunction);
+    ```
+
+2. **Mutations:**
+   - Mutations in React Query also involve caching. After a mutation is performed (e.g., updating data on the server), the corresponding query caches are automatically invalidated to reflect the changes.
+
+    ```jsx
+    const mutation = useMutation(updateDataFunction);
+    ```
+
+3. **Invalidate and Refetch:**
+   - You can manually invalidate and refetch data using the `invalidateQueries` and `refetchQueries` functions. This can be useful when you want to refresh specific data in response to user actions or external events.
+
+    ```jsx
+    const { data, refetch } = useQuery('myData', fetchDataFunction);
+
+    const handleButtonClick = () => {
+      // Manually trigger a refetch
+      refetch();
+    };
+    ```
+
+### Example of Setting a Time-Based Expiry:
+
+```jsx
+const { data, error, isLoading } = useQuery('myData', fetchDataFunction, {
+  // Set a time-based expiry of 5 minutes
+  staleTime: 5 * 60 * 1000,
+});
+```
+
+In this example, the `staleTime` option is used to define how long the data can be considered stale before triggering a refetch.
+
+Caching in React Query is designed to be intuitive and flexible, providing you with powerful tools to manage data fetching and ensure a smooth user experience in your applications. For more detailed information and examples, it's recommended to refer to the official React Query documentation: [React Query - Caching](https://react-query.tanstack.com/guides/query-caching).
+                                                                                                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                                                                                              ....
+
+------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+ Caching in React Query is a mechanism that stores the results of your queries in memory for a certain period of time. This allows for faster retrieval of data, reduces the number of requests made to your server, and provides a smoother user experience.
+
+Here's how caching works in React Query:
+
+1. **Storing and Retrieving Data:** When you make a query using `useQuery`, React Query automatically stores the result in an in-memory cache. If you make the same query again while the data is still in the cache, React Query will return the cached data instead of making a new network request.
+
+2. **Background Updates:** React Query performs background updates to keep the data in the cache fresh. When you re-render a component that uses a certain query, or when you come back to a page that uses a certain query, React Query will automatically refetch the data in the background to update the cache.
+
+3. **Stale Data and Refetching:** React Query marks cached data as "stale" as soon as it is used by a query. This means that React Query will try to refetch this data whenever it gets the chance (for example, when you refocus your browser window). You can configure how React Query handles stale data using the `staleTime` option in your query configuration.
+
+4. **Garbage Collection:** To prevent the cache from growing indefinitely, React Query removes unused data from the cache after a certain period of inactivity (by default, 5 minutes). You can configure this behavior using the `cacheTime` option in your query configuration.
+
+You should use caching in React Query whenever you're fetching data that you'll need to reuse across your application. This is especially useful for data that doesn't change often and can be expensive to fetch in terms of performance or cost (like API request limits).
+
+Here's an example of how to use caching in React Query:
+
+```jsx
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchData = async () => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return response.data;
+};
+
+export const MyComponent = () => {
+  const { isLoading, error, data } = useQuery('posts', fetchData);
+
+  if (isLoading) return 'Loading...';
+  if (error) return `An error occurred: ${error.message}`;
+
+  return (
+    <div>
+      {data.map(post => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+    </div>
+  );
+};
+```
+
+In this example, `useQuery` fetches data from an API and stores it in the cache with the key 'posts'. If `MyComponent` is rendered again, `useQuery` will first return the cached data (if it exists) and then refetch the data in the background to update the cache.
+
+
+
+
+
+
+
+
+  
