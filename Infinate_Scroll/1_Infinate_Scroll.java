9635b4b7728cb252234b780032d5a7940008c760
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-23 17:28:26 +0300

    Update 1_Infinate_Scroll.java

--- Infinate_Scroll/1_Infinate_Scroll.java
+++ Infinate_Scroll/1_Infinate_Scroll.java
@@ -1,3 +1,81 @@
+
+import React, { useEffect, useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import axios from 'axios';
+
+const pageSize = 10;
+
+const InfinitePosts = () => {
+ const [isLoadingMore, setIsLoadingMore] = useState(false);
+
+ const fetchPosts = async ({ pageParam }) => {
+   const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=${pageSize}`);
+   return response.data;
+ };
+
+ const { data, error, fetchNextPage, hasMore, isLoading, isFetchingNextPage } = useInfiniteQuery('posts', fetchPosts, {
+   getNextPageParam: (lastPage) => {
+     if (lastPage.length === 0) {
+       return undefined; // No more pages to fetch
+     }
+     return lastPage[lastPage.length - 1].id + 1; // Next page ID
+   },
+ });
+
+ const handleScroll = (event) => {
+   const { scrollTop, scrollHeight, clientHeight } = event.target;
+   if (scrollTop + clientHeight >= scrollHeight - 50 && !isLoadingMore && !isFetchingNextPage) {
+     setIsLoadingMore(true);
+     fetchNextPage().then(() => setIsLoadingMore(false));
+   }
+ };
+
+ useEffect(() => {
+   window.addEventListener('scroll', handleScroll);
+
+   // Clean up the event listener when the component unmounts
+   return () => {
+     window.removeEventListener('scroll', handleScroll);
+   };
+ }, []);
+
+ if (isLoading) {
+   return <div>Loading...</div>;
+ }
+
+ if (error) {
+   return <div>An error occurred: {error}</div>;
+ }
+
+ return (
+   <div>
+     {data.pages.map((page, pageIndex) => (
+       <div key={pageIndex}>
+         {page.map((item) => (
+           <div key={item.id}>{item.title}</div>
+         ))}
+       </div>
+     ))}
+
+     {hasMore && (
+       <button onClick={() => fetchNextPage()} disabled={isFetching}>
+         {isFetching ? 'Loading more...' : 'Load More'}
+       </button>
+     )}
+   </div>
+ );
+};
+
+export default InfinitePosts;
+
+
+
+
+
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
 Infinite queries in React Query refer to the practice of continuously fetching additional data as the user scrolls through a list or performs some action that triggers the loading of more data. This is often used in scenarios where you have a potentially endless list of items.
 
 ### Infinite Queries Example:
